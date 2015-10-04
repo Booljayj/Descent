c15768cf8045f7bd1ff3291e437bc1fad0b2d588
Justin <booljayj@gmail.com>
2015-10-04 11:30:20 -0800

    Add Screenshot Script

---
+++ Assets/Descent/Scripts/Screenshot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class Screenshot : MonoBehaviour {
+	public KeyCode key = KeyCode.F12;
+	public int scale = 1;
+
+	public static string ScreenShotName(int width, int height) {
+		return string.Format("screen_{0}x{1}_{2}.png",
+			                     width, height,
+			                     System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+	}
+
+	void LateUpdate() {
+		if (Input.GetKeyDown(key)) {
+			Capture();
+		}
+	}
+
+	public void Capture() {
+		Application.CaptureScreenshot(ScreenShotName(Screen.width, Screen.height), scale);
+		Debug.Log(string.Format("Saved new screenshot"));
+	}
+}
