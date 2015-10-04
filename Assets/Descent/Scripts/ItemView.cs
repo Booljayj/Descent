ccc838a45639bf3a4a7b03b253c337d713df5e32
Justin <booljayj@gmail.com>
2015-10-04 12:31:23 -0800

    Add Item View System
    Items respond to context changes and use animations.

---
+++ Assets/Descent/Scripts/ItemView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Image))]
+public class ItemView : MonoBehaviour {
+	public string itemName;
+
+	Animator animator;
+
+	void Awake() {
+		gameObject.SetActive(false);
+		animator = GetComponent<Animator>();
+	}
+
+	public void ContextChange(string key, int value) {
+		if (key.Equals(itemName)) {
+			if (value > 0) Show();
+			else Hide();
+		}
+	}
+
+	void Show() {
+		gameObject.SetActive(true);
+
+		if (animator) {
+			animator.SetBool("Show", true); //should be true by default, ensure it
+		}
+
+		transform.SetAsFirstSibling();
+	}
+
+	void Hide() {
+		if (animator) {
+			animator.SetBool("Show", false);
+		} else {
+			gameObject.SetActive(false);
+		}
+	}
+}
