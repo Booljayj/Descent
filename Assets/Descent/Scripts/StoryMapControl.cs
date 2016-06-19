d8cb216a1b25c62571a418bcb39c4bd54b87e1e6
Justin <booljayj@gmail.com>
2016-06-19 12:17:53 -0800

    Add ink integration for map control

---
+++ Assets/Descent/Scripts/StoryMapControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Ink.Runtime;
+
+public class StoryMapControl : MonoBehaviour {
+	[SerializeField] StoryReader _reader;
+	[SerializeField] string _variableName;
+	[SerializeField] MapScroll _scroller;
+
+	public void Awake() {
+		_reader.OnStoryLoaded += ObserveMapVariable;
+	}
+
+	void ObserveMapVariable(Story story) {
+		story.ObserveVariable(_variableName, MapVariableChanged);
+	}
+
+	void MapVariableChanged(string name, object value) {
+		_scroller.ShowZone((int)value);
+	}
+}
